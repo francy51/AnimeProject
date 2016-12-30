097d10fad0732b93bcd7bc05ad50bdef58d3078c
Francesco <francesco12382@gmail.com>
2016-12-31 02:45:04 +0800

    working on character control

---
+++ proj/RPG-NEW/Assets/Character Control System/CharacterControllerCustom.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using Project.StatSystem;
+using System;
+
+namespace Poject.CharacterControl
+{
+    public class CharacterControllerCustom : MonoBehaviour
+    {
+
+        playerStats stats;
+
+        [SerializeField]
+        float WalkSpeed;
+
+        Rigidbody rb;
+
+        [SerializeField]
+        float Hrizontal;
+        [SerializeField]
+        float Vertical;
+        [SerializeField]
+        float Jump;
+        [SerializeField]
+        Vector3 Displacement;
+        [SerializeField]
+        LayerMask Ground;
+        [SerializeField]
+        float JumpSpeed;
+        [SerializeField]
+        bool isGrounded;
+        [SerializeField]
+        float Gravity = 10f;
+
+        Ray ray;
+
+        // Use this for initialization
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            Physics.gravity = new Vector3(0, -Gravity * rb.mass);
+            stats = FindObjectOfType<playerStats>().GetComponent<playerStats>();
+            WalkSpeed = stats.WalkSpeed;
+            JumpSpeed = stats.JumpHeight;
+            Displacement = new Vector3();
+        }
+
+        private void Update()
+        {
+            ray = new Ray(transform.position, Vector3.down);
+            checkGrounded();
+        }
+
+
+        void FixedUpdate()
+        {
+            GatherInput();
+            AddInputsTogether();
+            MoveCharacter();
+        }
+
+        private void MoveCharacter()
+        {
+            rb.velocity = Displacement;
+        }
+
+        private void AddInputsTogether()
+        {
+            Displacement = new Vector3(Hrizontal, Jump, Vertical);
+        }
+
+        void GatherInput()
+        {
+
+            Hrizontal = Input.GetAxis("Horizontal") + (Input.GetAxis("Horizontal") * WalkSpeed);
+            Vertical = Input.GetAxis("Vertical") + (Input.GetAxis("Vertical") * WalkSpeed);
+            Jump = Input.GetAxisRaw("Jump") * JumpSpeed;
+
+            if (isGrounded)
+            {
+                Jump = Input.GetAxisRaw("Jump") * JumpSpeed;
+                print(isGrounded);
+            }
+
+        }
+
+        private void checkGrounded()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, JumpSpeed))
+            {
+                Debug.DrawLine(ray.origin, hit.point, Color.red);
+                if (hit.collider.tag == "Ground")
+                {
+                    isGrounded = true;
+                    print("enter Loop");
+                }
+            }
+            else
+            {
+                isGrounded = false;
+            }
+        }
+    }
+}
---
+++ proj/RPG-NEW/Assets/Character Creation System/controllers/animationController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+namespace Project.CharacterCreation
+{
+    public class animationController : MonoBehaviour
+    {
+
+        public Animator FemaleAnim;
+        public Animator MaleAnim;
+        public GameObject male;
+        public GameObject female;
+        float animSpeed;
+  
+        CharacterCreationManager manager;
+
+        // Use this for initialization
+        void Start()
+        {
+            manager = GetComponent<CharacterCreationManager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (manager.choosingCharacter)
+            {
+                FemaleAnim.SetFloat("Blend", 1f);
+                MaleAnim.SetFloat("Blend", 1f);
+            }
+        }
+
+        public GameObject characterSelected (GameObject trigger)
+        {
+            if (trigger.name == male.name)
+            {
+                MaleAnim.SetBool("chosenCharacter", true);
+                female.SetActive(false);
+                return male;
+            }
+            else if (trigger.name == female.name)
+            {
+                FemaleAnim.SetBool("chosenCharacter", true);
+                male.SetActive(false);
+                return female;
+            }
+            else
+                return null;
+        }
+
+        public void ResetCharacters()
+        {
+            MaleAnim.SetBool("chosenCharacter", false);       
+            FemaleAnim.SetBool("chosenCharacter", false);
+            FemaleAnim.SetFloat("Blend", 1f);
+            MaleAnim.SetFloat("Blend", 1f);
+            male.SetActive(true);
+            female.SetActive(true);
+        }
+
+    }
+}
